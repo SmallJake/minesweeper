b756159d62e350ea1f74df18e04163433e7f50a3
Jake <jake_small@brown.edu>
2015-05-10 17:51:39 -0400

    Added a timer

--- minesweeper.js
+++ minesweeper.js
@@ -2,20 +2,21 @@ coveredSquare = "images\\square-large.png"
 flagSquare = "images\\flag-large.png"
 mineSquare = "images\\mine.png"
 
-const EASY = 0.1
-const MID = 0.2
-const HARD = 0.3
-
-const MIDWIDTH = 500
+const MINWIDTH = 700
 const MINSIZE = 20
 
-mySize = 0
+var mySize
 
-myRows = 0
-myCols = 0
-myDiff = EASY
+var myRows
+var myCols
+var myDiff
 
-myFirstClick = true
+var myFirstClick
+var myGoal
+
+var myTimerStartStop
+
+var myTimer
 
 myBoard = []
 
@@ -27,9 +28,40 @@ function Square(){
 	this.adjacentMines = 0
 }
 
+//Represents a location on the playing field
+function Point(r, c){
+	this.row = r
+	this.col = c
+}
+
+// Holds a time
+function Timer(){
+	this.seconds = 0
+	this.minutes = 0
+	this.hours = 0
+}
+
+//Displays a timer in the form m:ss, or h:mm:ss, if large enough
+Timer.prototype.toString = function(){
+	var timeString = myTimer.seconds.toString()
+	if (timeString.length == 1)
+		timeString = "0" + timeString
+	timeString = myTimer.minutes.toString() + ":" + timeString
+	if (myTimer.hours != 0){
+		if (timeString.length == 4)
+			timeString = "0" + timeString
+		timeString = myTimer.hours.toString() + ":" + timeString
+	}
+	return timeString
+}
+
 //Shows the menu by setting it visible
 function showMenu(){
+	document.getElementById("field").innerHTML = ""
+	document.getElementById("result").style.display="none"
 	document.getElementById("menu").style.display = "block"
+	document.getElementById("options").style.display = "none"
+	window.clearInterval(myTimerStartStop)
 }
 
 //Triggered when the player hits the start game button
@@ -67,11 +99,18 @@ function submit(){
 		startGame()
 }
 
-//Initiates the board & draws the field
+//Initializes the board & draws the field
 function startGame(){
 	document.getElementById("menu").style.display = "none"
-	mySize = Math.max(MINSIZE, MIDWIDTH/myCols)
-	document.getElementById("field").style.width = mySize * myCols + 10
+	document.getElementById("result").style.display="none"
+	mySize = Math.max(MINSIZE, MINWIDTH/Math.max(myCols, myRows))
+	document.getElementById("field").style.width = mySize * myCols
+	
+	document.getElementById("options").style.width = Math.min(MINWIDTH, mySize * myCols)
+	document.getElementById("options").style.display = "block"
+
+	myTimer = new Timer()
+	document.getElementById("timer").innerHTML = "0:00"
 
 	for (var i = 0; i < myRows; i++){
 		var row = []
@@ -85,12 +124,12 @@ function startGame(){
 	drawField()
 }
 
-//Draws all the squares
+//Draws all the squares of the field
 function drawField(){
 	var field = ""
 	for (var i = 0; i < myRows; i++){
 		for (var j = 0; j < myCols; j++){
-			field += "<img id =\"" + i + " " + j + "\" onclick=\"handleClick(" + i + "," + j + ")\" src=" + coveredSquare + " width = \"" + mySize + "\" height = \"" + mySize + "\">"
+			field += "<img id =\"" + i + " " + j + "\" onclick=\"handleClick(new Point(" + i + "," + j + "))\" src=" + coveredSquare + " width = \"" + mySize + "\" height = \"" + mySize + "\">"
 		}
 		field += "<br>\n"
 	}
@@ -98,52 +137,58 @@ function drawField(){
 }
 
 // Randomly places mines on the board, excluding the given square
-function layMines(row, col){
+function layMines(point){
 	var squaresLeft = myRows * myCols
-	var totalMines = squaresLeft * myDiff
+	var totalMines = Math.floor(squaresLeft * myDiff)
+	myGoal = squaresLeft - totalMines
 	var mines = 0
 	squaresLeft--
 	for (var r = 0; r < myRows; r++){
 		for (var c = 0; c < myCols; c++){
-			if (r == row && c == col)
+			if (r == point.row && c == point.col)
 				continue
 			chance = (totalMines - mines)/squaresLeft
 			squaresLeft--
 			if (Math.random() < chance){
 				myBoard[r][c].isMine = true
 				mines++
-				applyAround(r, c, increaseMineCount)
+				applyAround(new Point(r, c), increaseMineCount)
 			}
 		}
 	}
 }
 
-function applyAround(row, col, func){
-	var minRow = Math.max(row - 1, 0)
-	var maxRow = Math.min(row + 1, myRows - 1)
-	var minCol = Math.max(col - 1, 0)
-	var maxCol = Math.min(col + 1, myCols - 1)
+//Takes a point & a function which can be applied to a point, & applies that function to every point adjacent to the given point
+function applyAround(point, func){
+	var minRow = Math.max(point.row - 1, 0)
+	var maxRow = Math.min(point.row + 1, myRows - 1)
+	var minCol = Math.max(point.col - 1, 0)
+	var maxCol = Math.min(point.col + 1, myCols - 1)
 	for (var m = minRow; m <= maxRow; m++){
 		for (var n = minCol; n <= maxCol; n++) {
-			var x = m
-			var y = n
-			if (!(m == row && n == col))
-				func(m, n)
+			if (!(m == point.row && n == point.col))
+				func(new Point(m, n))
 		}
 	}
 }
 
-function increaseMineCount(row, col){
-	myBoard[row][col].adjacentMines++
+//Adds one to a square's adjacent mine count.
+function increaseMineCount(point){
+	myBoard[point.row][point.col].adjacentMines = myBoard[point.row][point.col].adjacentMines + 1
 }
 
 //Called whenever a covered square is clicked, takes an object with the coordinates of the clicked square
-function handleClick(row, col) {
-	if (myFirstClick) {
-		firstClick(row, col)
+function handleClick(point) {
+	if(event.ctrlKey){
+		var square = myBoard[point.row][point.col]
+		square.isFlagged = !square.isFlagged
+		document.getElementById(point.row + " " + point.col).src = square.isFlagged ? flagSquare : coveredSquare
 	}
 	else {
-		otherClick(row, col)
+		if (myFirstClick) {
+			firstClick(point)
+		}
+		reveal(point)
 	}
 }
 
@@ -151,55 +196,96 @@ function handleClick(row, col) {
 //Toggles whether a square has a flag on ctrl-click
 //On a regular click distributes mines across the board, making sure the given square is not a mine
 //This function ensures that a player does not lose on their first click, since that would seem unfair
-function firstClick(row, col){
-	if(event.ctrlKey){
-		var square = myBoard[row][col]
-		square.isFlagged = !square.isFlagged
-		document.getElementById(row + " " + col).src = square.isFlagged ? flagSquare : coveredSquare
-	}
-	else {
-		myFirstClick = false
-		layMines(row, col)
-		otherClick(row, col)
-	}
+function firstClick(point){
+	myTimerStartStop = setInterval(tickTock, 1000)
+	myFirstClick = false
+	layMines(point)
 }
 
 //Called for each click of an uncovered square, takes an object with the coordinates of the clicked square
 //Toggles whether a square has a flag on ctrl-click
 //On a regular click uncovers a square
-function otherClick(row, col) {
-	var square = myBoard[row][col]
-	if(event.ctrlKey){
-		square.isFlagged = !square.isFlagged
-		document.getElementById(row + " " + col).src = square.isFlagged ? flagSquare : coveredSquare
-	}
-	else {
-		if (!square.isFlagged){
-			reveal(row, col)
+function reveal(point) {
+	var square = myBoard[point.row][point.col]
+	if (!square.isFlagged){
+		if (!square.isRevealed){
+			square.isRevealed = true
+
+			var boardPiece = document.getElementById(point.row + " " + point.col)
+			var adj = square.adjacentMines
+
+			if (square.isMine){
+				boardPiece.src = mineSquare
+				gameover()
+			}
+			else{
+				boardPiece.src = "images\\" + adj + ".png"
+				if (adj == 0)
+					applyAround(point, reveal)
+				boardPiece.onclick = null
+				myGoal--
+				if (myGoal == 0)
+					win()
+			}
 		}
 	}
 }
 
-function reveal(row, col) {
-	var square = myBoard[row][col]
-	if (!square.isRevealed){
-		square.isRevealed = true
+//Called once a second during gameplay
+function tickTock(){
+	addSecond(myTimer)
+	document.getElementById("timer").innerHTML = myTimer.toString()
+}
 
-		var boardPiece = document.getElementById(row + " " + col)
-		var adj = square.adjacentMines
-
-		if (square.isMine){
-			boardPiece.src = mineSquare
+//Adds a second to the timer, taking care of wraparound
+function addSecond(timer){
+	timer.seconds++
+	if (timer.seconds >= 60) {
+		timer.seconds -= 60
+		timer.minutes++
+		if (timer.minutes >= 60) {
+			timer.minutes -= 60
+			timer.hours++
 		}
-		else{
-			boardPiece.src = "images\\" + adj + ".png"
-		}
-
-		if (adj == 0)
-			applyAround(row, col, reveal)
-
-		boardPiece.onclick = null
 	}
 }
 
-showMenu()
+//Disables all the square on the field so that they cannot be clicked on
+function disableButtons(){
+	for (var i = 0; i < myRows; i++){
+		for (var j = 0; j < myCols; j++){
+			document.getElementById(i + " " + j).onclick = null
+		}
+	}
+}
+
+//Stops the timer, disables the board, & shows the result screen
+function endgame(){
+	window.clearInterval(myTimerStartStop)
+	disableButtons()
+	document.getElementById("result").style.display = "block"
+}
+
+//Informs the player they lost
+function gameover(){
+	document.getElementById("resulthead").innerHTML = "Too bad!"
+	document.getElementById("resultbody").innerHTML = "You lose."
+	revealMines()
+	endgame()
+}
+
+function revealMines(){
+	for (var i = 0; i < myRows; i++){
+		for (var j = 0; j < myCols; j++){
+			if (myBoard[i][j].isMine)
+				document.getElementById(i + " " + j).src = mineSquare
+		}
+	}
+}
+
+//Informs the player they won
+function win(){
+	document.getElementById("resulthead").innerHTML = "Congratulations!"
+	document.getElementById("resultbody").innerHTML = "You won in " + myTimer.toString() + "."
+	endgame()
+}
